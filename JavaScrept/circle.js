145839e8afecd80c59f452aa8f0fc32e0e43c814
sa3dalla <ugs.160345@eng.suez.edu.eg>
2023-03-11 11:53:21 +0200

    Update circle.js

--- JavaScrept/circle.js
+++ JavaScrept/circle.js
@@ -4,5 +4,5 @@ const text = document.getElementById("text");
 text.innerHTML = text.textContent.replace(/\S/g, "<span>$&</span>");
 const ele = document.querySelectorAll("span");
 for (var i = 0; i < ele.length; i++) {
-  ele[i].style.transform = "rotate(" + i * 15 + "deg)";
+  ele[i].style.transform = "rotate(" + i * 30 + "deg)";
 }
